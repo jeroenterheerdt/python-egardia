acf4935f495c3cc45d095fd667cc1d8b8105ec3f
jeroen <jeroen_ter_heerdt@hotmail.com>
2017-07-06 19:48:03 +0200

    First release

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='python-egardia',
+      version='1.0.0',
+      description='Python 3 support for Egardia / Woonveilig alarm',
+      url='https://github.com/jeroenterheerdt/python-egardia',
+      author='Jeroen ter Heerdt',
+      license='MIT',
+      install_requires=['requests>=2.0'],
+      tests_require=['mock'],
+      test_suite='tests',
+      packages=find_packages(exclude=["dist", "*.test", "*.test.*", "test.*", "test"]),
+      zip_safe=True)
