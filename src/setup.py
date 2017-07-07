52988aa2ef350823c6d70ec34d048300af6801ea
jeroen <jeroen_ter_heerdt@hotmail.com>
2017-07-07 14:06:16 +0200

    Updated to version 1.0.1

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='python-egardia',
-      version='1.0.0',
+      version='1.0.1',
       description='Python 3 support for Egardia / Woonveilig alarm',
       url='https://github.com/jeroenterheerdt/python-egardia',
       author='Jeroen ter Heerdt',
