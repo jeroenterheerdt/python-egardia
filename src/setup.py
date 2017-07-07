4d407a4dd7349d41a079f9640391571817f81e01
jeroen <jeroen_ter_heerdt@hotmail.com>
2017-07-07 14:33:18 +0200

    Fixing package names

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='python-egardia',
-      version='1.0.1',
+      version='1.0.2',
       description='Python 3 support for Egardia / Woonveilig alarm',
       url='https://github.com/jeroenterheerdt/python-egardia',
       author='Jeroen ter Heerdt',
