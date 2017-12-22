acb9155b8f8e579180f4409a9855d24f45a37512
Martin Hjelmare <marhje52@kth.se>
2017-12-22 20:24:37 +0100

    Refactor egardiaserver
    * Remove home assistant interface.
    * Use socketserver module.
    * Allow registering callbacks. Callbacks should accept a dict as single
      argument. The dict will contain the new status of the alarm.

--- src/pythonegardia/egardiaserver.py
+++ src/pythonegardia/egardiaserver.py
@@ -1,152 +1,104 @@
-"""
-Egardia / Woonveilig server that passes along alarms
-"""
-import socket
+"""Egardia / Woonveilig server that passes along alarms."""
 import argparse
-import sys
 import logging
-import datetime
-import homeassistant.remote as remote
-import time
+import socketserver
+import threading
 
 _LOGGER = logging.getLogger(__name__)
 
+DEFAULT_RESPONSE = "HTTP/1.1 200 OK\n\nHello, World!"
 
-class EgardiaServer(object):
-    """
-    Egardia server passing along alarms
-    """
-    def __init__(self, port, hasshost, password, haport, ssl, retrycount, waittime):
-        self._port = port
-        self._hasshost = hasshost
-        self._password = password
-        self._haport = haport
-        self._ssl = ssl
-        self._retrycount = retrycount
-        self._waittime = waittime
-        msg = "Sleeping to give Home Assistant a moment to start"
-        _LOGGER.info(msg)
-        print(msg)
-        time.sleep(waittime)
+
+class EgardiaServer(socketserver.TCPServer, threading.Thread):
+    """Egardia server passing along alarms."""
+
+    def __init__(self, host, port):
+        """Prepare server."""
+        threading.Thread.__init__(self)
+        self._address = (host, port)
+        self.callbacks = []
+        socketserver.TCPServer.__init__(
+            self, self._address, EgardiaServerHandler, bind_and_activate=False)
+        self.allow_reuse_address = True
+        self.status = ''
+
+    def bind(self):
+        """Bind and listen to address."""
         try:
-            validationresult = "init"
-            numretries = 0
-            while validationresult != "ok" and numretries <= self._retrycount:
-                self._api = remote.API(self._hasshost, self._password,
-                                       self._haport,self._ssl)
-                validationresult = str(remote.validate_api(self._api))
-                numretries = numretries+1
-                if validationresult != "ok":
-                   msg = "Connecting to Home Assistant failed, "
-                   msg = msg + "retrying..."
-                   _LOGGER.info(msg)
-                   print(msg)
-                   time.sleep(5)
-            if validationresult != "ok":
-               msg = "Error connection to Home Assistant: "
-               msg = msg + validationresult
-               _LOGGER.error(msg)
-               print(msg)
-               sys.exit(2)
-            else:
-               msg = "Succesfully connected to Home Assistant"
-               print(msg)
-               _LOGGER.info(msg)
-        except:
-            e = sys.exc_info()[0]
-            msg = "Error connecting to Home Assistant: "
-            msg = msg + str(e)+", please check settings."
-            print(msg)
-            _LOGGER.error(msg)
-            sys.exit(2)
-        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.server_bind()
+            self.server_activate()
+        except OSError:
+            self.server_close()
+            _LOGGER.error(
+                "Can't bind to address %s", self._address)
+            return
+
+        _LOGGER.info("Server listening on %s", self._address)
+
+    def register_callback(self, func):
+        """Store callback in list.
+
+        The callback should accept a dict as argument. For each request from
+        the alarm control panel, all callbacks will be called with a dict
+        containing the new status of the alarm.
+        """
+        self.callbacks.append(func)
+
+    def notify(self, data):
+        """Call callbacks with event data."""
         try:
-            listen_socket.bind(('', self._port))
-            listen_socket.listen(1)
-        except:
-            msg = "Permission error, make sure to run "
-            msg = msg + "this as root or in sudo mode"
-            print(msg)
-            _LOGGER.error(msg)
-            sys.exit(2)
-        msg = "EgardiaServer listening on port "+str(port)
-        _LOGGER.info(msg)
-        print(msg)
-        self._status = ""
-        while True:
-            client_connection, client_address = listen_socket.accept()
-            request = client_connection.recv(1024)
+            for func in self.callbacks:
+                _LOGGER.debug(
+                    "Notify callback with data: %s", data)
+                func(data)
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Could not notify callback")
 
-            http_response = """\
-HTTP/1.1 200 OK
+    def run(self):
+        """Run the server."""
+        _LOGGER.info('Starting server %s', self._address)
+        self.serve_forever()
 
-Hello, World!
-"""
-            requestdecoded = request.decode('utf8')
-            if requestdecoded.startswith("["):
-                newstatus = requestdecoded[requestdecoded.index(' ')+1:]
-                newstatus = newstatus[:len(newstatus)-1]
-                msg = "["+str(datetime.datetime.now())+"] Received new "
-                msg = msg + "statuscode from alarm system: "+newstatus
-                _LOGGER.info(msg)
-                print(msg)
-                if newstatus != self._status:
-                    self._status = newstatus
-                    payload = {"status": self._status}
-                    try:
-                        remote.fire_event(self._api,
-                                          'egardia_system_status',
-                                          data=payload)
-                        msg = "egardia_system_status event fired with payload: "
-                        msg = msg + str(payload)
-                        _LOGGER.info(msg)
-                        print(msg)
-                    except:
-                        e = sys.exc_info()[0]
-                        msg = "Could not fire event, is your "
-                        msg = msg +"HASS server running?"
-                        _LOGGER.error(msg)
-                        print(msg)
-                        _LOGGER.error(str(e))
-                        print(str(e))
-            client_connection.sendall(http_response.encode('utf8'))
-            client_connection.close()
+    def stop(self):
+        """Stop the server."""
+        _LOGGER.info('Stopping server %s', self._address)
+        self.shutdown()
 
 
-def main(argv):
-    parser = argparse.ArgumentParser(description='Run the EgardiaServer')
-    parser.add_argument('-port', help='the port number to run the server ' +
-                        'on (defaults to 52010)', default='52010')
-    parser.add_argument('-host', help='the host of the Home Assistant ' +
-                        'server (defaults to localhost)',
-                        default='127.0.0.1')
-    parser.add_argument('-password', help='the password for Home Assistant ' +
-                        '(default none)', default='')
-    parser.add_argument('-haport', help='the port number for the Home '+
-                        'Assistant server (defaults to 8123)',
-                        default='8123')
-    parser.add_argument('-ssl', help='connect to Home Assistant through '+
-                        'a secure channel (defaults to False)',
-                        default=False)
-    parser.add_argument('-numretry', help='maximum number of retries '+
-                        'connecting to Home Assistant (defaults to 10)',
-                        default='10')
-    parser.add_argument('-waittime',help='number of seconds to wait '+
-                        'before connection to Home Assistant for the '+
-                        'first time to give Home Assistant time to '+
-                        'start (defaults to 120)',
-                        default='120')
+class EgardiaServerHandler(socketserver.BaseRequestHandler):
+    """Egardia server handler passing along alarms."""
+
+    def handle(self):
+        """Handle messages to this server."""
+        # self.request is the TCP socket connected to the client
+        data = self.request.recv(1024).strip()
+        data = data.decode('utf8')
+        if data.startswith('['):
+            new_status = data[data.index(' ') + 1:]
+            new_status = new_status[:-1]
+            _LOGGER.debug(
+                "Received new status code from alarm system: %s", new_status)
+            if self.server.status != new_status:
+                self.server.status = new_status
+                # self.server is the socketserver
+                self.server.notify({'status': new_status})
+        self.request.sendall(DEFAULT_RESPONSE.encode('utf8'))
+
+
+def main():
+    """Main function."""
+    parser = argparse.ArgumentParser(description="Run the EgardiaServer")
+    parser.add_argument(
+        '-P', '--port', default='52010', type=int,
+        help="The port number to run the server on (defaults to 52010)")
     args = parser.parse_args()
     port = args.port
-    host = args.host
-    password = args.password
-    haport = args.haport
-    ssl = args.ssl
-    retrycount = args.numretry
-    waittime = args.waittime
-    EgardiaServer(int(port), host, password, int(haport), bool(ssl),
-                  int(retrycount),int(waittime))
+    host = 'localhost'
+    server = EgardiaServer(host, port)
+    server.bind()
+    server.start()
+    # server.stop()
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+
+if __name__ == '__main__':
+    main()
