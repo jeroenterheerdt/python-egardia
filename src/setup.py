408e1ad189f8588e376c1a03f7701ad1361179dd
SneakSnackSnake <55602459+SneakSnackSnake@users.noreply.github.com>
2019-09-24 22:31:19 +0200

    Fix for WV-1716
    WV-1716 needs the ACKNOWLEDGE unicode character as a response, otherwise it keeps sending the same code.

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pythonegardia',
-      version='1.0.39',
+      version='1.0.40',
       description='Python 3 support for Egardia / Woonveilig alarm',
       url='https://github.com/jeroenterheerdt/python-egardia',
       author='Jeroen ter Heerdt',
