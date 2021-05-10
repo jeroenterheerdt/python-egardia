614b08119b679ba92b3a65d65f3c7dee2e0f6689
Jeroen ter Heerdt <jterh@microsoft.com>
2021-05-10 09:41:02 -0700

    adding manifest and fixing setup version

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='pythonegardia',
-      version='1.0.40',
+      version='1.0.50',
       description='Python 3 support for Egardia / Woonveilig alarm',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/jeroenterheerdt/python-egardia',
       author='Jeroen ter Heerdt',
       license='MIT',
@@ -10,4 +14,5 @@ setup(name='pythonegardia',
       tests_require=['mock'],
       test_suite='tests',
       packages=find_packages(exclude=["dist", "*.test", "*.test.*", "test.*", "test"]),
+      include_package_data=True,
       zip_safe=True)
